1f88c6fc42c8bbc10e964529dbc8d0877d4a7c41
Rabindra Tamang <rabee.tmg123@gmail.com>
2021-10-18 08:05:38 +0545

    static configuration

--- Backend/urls.py
+++ Backend/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf  import settings
+from django.conf.urls.static import static
 
 
 
 urlpatterns = [
     path('private/', admin.site.urls),
     path("",include('Api.urls'), name="menulist"),
-]
+] + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
